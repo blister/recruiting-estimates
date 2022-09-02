22db07d251ae021c5f4ad7030483e3d6350e5bcb
Eric Harrison <me@ericharrison.info>
2022-09-02 19:28:14 -0400

    First pass at template

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const app = express();
 
+app.use(express.static('static'));
+app.set('view engine', 'ejs');
+
+app.use((req, res, next) => {
+    res.locals.is_admin = false;
+    next();
+});
+
 app.get('/', (req, res) => {
-    res.send('Hello world');
+    res.render('dashboard');
 });
 
 app.listen(process.env.PORT || 3001);
