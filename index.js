90867f34cd4fc4ffe768dbf1d172f35b98ebae44
Eric Harrison <me@ericharrison.info>
2022-09-02 20:12:23 -0400

    Initial pass of calculator

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 
+app.use(express.urlencoded({extended: true });
 app.use(express.static('static'));
 app.set('view engine', 'ejs');
 
@@ -11,6 +12,10 @@ app.use((req, res, next) => {
 
 app.get('/', (req, res) => {
     res.render('dashboard');
+});
+
+app.post('/estimate', (req, res) => {
+    res.json(req.body);
 });
 
 app.listen(process.env.PORT || 3001);
